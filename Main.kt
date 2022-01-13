385ca6c23cabe4a2772d360b4095662b8f3a3594
Mike Peterson <mpeterson2004@gmail.com>
2022-01-12 18:17:18 -0700

    final edits and commits prior to submit

--- Main.kt
+++ Main.kt
@@ -31,38 +31,49 @@ class Square(): Shape() {
 
 //inherited increaseSize method
     override fun increaseSize(percentage: Float) {
+    // it would be more accurate to increase this.side by percentage and then recalculate area but as I wasn't planning on doing that for circle I opted not to do it here for consistency
         area *= (1f+percentage)
     }
 
 
 }
 
+class Circle(): Shape() {
+
+
+    //default initialization
+    var radius:Float = 1f
+    override var area: Float = 0.0f
+    override var name: String = "Circle"
+    init {
+        area = (radius*radius)*3.14159f
+
+    }
+    constructor(radius:Float) : this() {
+        this.radius = radius
+        this.area = (radius*radius)*3.14159f
+
+    }
+
+    //inherited increaseSize method
+    override fun increaseSize(percentage: Float) {
+        //as we are only ever dealing with the area of the circle I am only increasing the area I could back-solve by taking the modified area dividing by pi and reassigning the sqrt of that value to radius but for the purposes described I thought this was the most elegant solution
+        area *= (1f+percentage)
+    }
+
+
+}
+
+
 fun testingGround(){
-    var i = 10
     var shapes = mutableListOf<Shape>()
-    while (i>0){
-        i--
-        shapes.add(Square(i.toFloat()))
-        println("the loop has $i processes remaining")
-        for(shape in shapes){
-            println("${shape.name} is ${shape.area}^2")
-            shape.increaseSize(.1f)
-        }
+    shapes.add(Square(3f))
+    shapes.add(Circle(2f))
+
+    //originally was going to format the area to 2 decimals but as it is specifically requested to keep the code as simple as possible I decided against it
+    for (shape in shapes){
+        println("Prior to edit ${shape.name} has an area of ${shape.area}")
+        shape.increaseSize(.1f)
+        println("after size increase ${shape.name} has an area of ${shape.area}")
     }
 }
-/*
-Please complete the following coding assignment.
-
-Create a Square class derived from Shape with a default side length of 1
-Create a Circle class derived from Shape with a default radius of 1
-The area property should return the area of the shape
-The name property should return the name of the shape (“Square” or “Circle”)
-The increaseSize method should take a percentage parameter and increase the size of the shape by that percentage
-Create a collection of Shapes
-Insert a Square object into the Shapes collection with a side length of 3
-Insert a Circle into the Shapes collection with a radius of 2
-Use a loop to do the following for each shape:
-Print the name and area of the shape
-Increase the size of the shape by 10%
-Print the name and new area of the shape.
-*/
